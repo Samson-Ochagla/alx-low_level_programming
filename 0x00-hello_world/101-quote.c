551a326bd343f896dd2d513e46e64c36fd653f4d
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-12 05:42:33 -0800

    C program that prints a text to standard error without using puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			59);
-	return (1);
-	
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			stderr);
+	return(1);
 }
