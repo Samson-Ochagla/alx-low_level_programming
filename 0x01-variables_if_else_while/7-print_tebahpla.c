32b405deb2a375e0b44a891e745de36a698092b1
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-12 21:00:09 -0800

    C program that prints the lowercase alphabet in reverse, followed by a new linegit push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints the lowercase alphabet in reverse
+ * You can only use the putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
