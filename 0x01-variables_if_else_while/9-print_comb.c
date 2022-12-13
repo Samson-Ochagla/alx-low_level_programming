68f95d55ef1881c97ab9d3572aaf126785ef71fc
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-12 22:16:50 -0800

    C program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,6 +3,7 @@
  * main - Program that prints all possible combinations of single-digit numbers
  * Seperated by ,, followed by a space and printed in ascending order
  * You can only use the putchar function
+ * Return: Aways 0 (Success)
  */
 int main(void)
 {
@@ -10,7 +11,7 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		putchar((num % 10 ) + '0');
+		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
 		putchar(',');
