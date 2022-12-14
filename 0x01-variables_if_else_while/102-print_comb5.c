aacf1b161a3671d53c042cd03f54e6013530f23c
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-13 20:15:05 -0800

    C program that prints all possible combinations of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main- Program that prints all possible combinations of two two-digit numbers,
+ * main- Program that prints all possible combinations of two two-digits
  * ranging from 0-99, separated by a comma followed by a space
  * Return: Always 0 (Success)
  */
