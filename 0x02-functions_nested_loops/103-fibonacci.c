6230f0da3a4789e66dd4521dee9751b01d3407ad
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-16 03:43:28 -0800

    C progrm that prints the sum of even-valued Fibonacci sequence not exceeding 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the sum of even-valued terms of
+ * Fibonacci sequence terms not exceeding 4000000
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long f1 = 0. f2 = 1, n;
+
+	while (n < 4000000)
+	{
+		n = f1 + f2;
+		if ((n % 2) == 0)
+		{
+			n += f1;
+		}
+	}
+	printf("%ld\n", n);
+	return (0);
+}
