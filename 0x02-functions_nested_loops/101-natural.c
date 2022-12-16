915bc688c8589bebb0c63579fee17e4b0c7eab69
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-16 01:15:59 -0800

    C program that prints natural numbers of multiples of 3 and 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,12 +5,8 @@
  *  Return: Always 0 (Success)
  */
 int main(void)
-{
-<<<<<<< HEAD
+
 	int i, sum = 0;
-=======
-	int i; sum = 0;
->>>>>>> 9b838fb2246af84b1947e3f52854e9b9f121cf70
 
 	for (i= 0; i < 1024; i++)
 	{
