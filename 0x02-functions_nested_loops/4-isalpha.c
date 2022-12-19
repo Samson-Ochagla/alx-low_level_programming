4e9874cea19ec0a5df7881bd1eb59791d5dbd804
Samson-Ochagla <ochaglasamson@gmail.com>
2022-12-19 04:46:55 -0800

    C function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if (c == 'a' || c >= 'a' && c <= 'z')
+	if ((c == 'a' || c >= 'a') && (c <= 'z'))
 		return (1);
-	else if (c == 'A' || c>= 'A' && c <= 'Z')
+	else if ((c == 'A' || c>= 'A') && (c <= 'Z'))
 		return(1);
 	else
 		return (0);
