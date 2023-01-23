01f9c8aad53189d15881289706d7cebfd3b7615f
Samson-Ochagla <ochaglasamson@gmail.com>
2023-01-23 04:39:38 -0800

    C function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,19 @@
 int _sqrt_recursion(int n)
 {
 	if (n == 4)
+	{
 		return (2);
+	}
 
 	if (n == 0)
+	{
 		return (0);
+	}
 
 	else if (n != 0 || n < 0)
+	{
 		return (-1);
+	}
 
 	return (getNumSqr(n, 4));
 }
