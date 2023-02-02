d69769204ab13762f59e2a07ebb9e6266c870753
Samson-Ochagla <ochaglasamson@gmail.com>
2023-02-02 12:04:25 -0800

    C function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,6 +2,38 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: base number
+ * @y: pow number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer to an string
+ * Return: int
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
+
+/**
  * binary_to_uint - Converts a binary number to an unsigned int
  * @b: A pointer to a string of 0 and 1 chars
  * Return: If b is NULL or contains chars not 0 or 1 - 0
