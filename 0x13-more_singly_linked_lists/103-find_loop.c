414438d76e6deca5c60720fb4db460a77eddc907
Samson-Ochagla <117741117+Samson-Ochagla@users.noreply.github.com>
2023-02-02 12:30:23 +0100

    C function that return the address of the node where the loop starts or NULL where there is no loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,7 +5,7 @@
  * find_listint_loop - Finds the loop contained in
  * a listint_t linked list
  * @head: A pointer to the head of the listint_t list
- * Return: Returns: The address of the node where the loop starts
+ * Return: The address of the node where the loop starts
  * or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
